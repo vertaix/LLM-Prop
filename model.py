c64e226b4bc88e30d1995e4051d8e7d3704a03e1
Andrews2017 <3071003670@qq.com>
2022-10-23 15:50:32 -0400

    summing up the embeddings of the outputed characters from ByT5

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 ByT5 finetuning on materials property prediction using materials textual description 
 """
 # Import packages
+import torch
 import torch.nn as nn
 
 class ByT5Reggressor(nn.Module):
@@ -24,12 +25,17 @@ class ByT5Reggressor(nn.Module):
         print(hidden_states)
         print("-"*20)
 
-        last_hidden_state = hidden_states.last_hidden_state
+        last_hidden_state = hidden_states.last_hidden_state # [batch_size, input_length, D_in]-->ByT5_output
         print("last_hidden_state_shape = ", last_hidden_state.size())
         print(last_hidden_state)
         print("-"*20)
 
-        outputs = self.regressor(last_hidden_state) # Feed the regression model only the last  hidden state of ByT5 model
+        input_embedding = torch.sum(last_hidden_state, 1) # [batch_size, D_in] --> getting the embedding of the output by summing up the embeddings of the output characters 
+        print("input_embedding = ", input_embedding.size())
+        print(input_embedding)
+        print("-"*20)
+
+        outputs = self.regressor(input_embedding) # [batch_size, D_out] -->Feed the regression model only the last  hidden state of ByT5 model
         print("outputs_shape = ", outputs.size())
         print(outputs)
         print("-"*20)
