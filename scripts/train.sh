1925ccbeb1431c6bd2c5ab1457a11e3d32395494
Andrews2017 <3071003670@qq.com>
2022-10-25 18:29:12 -0400

    added scripts for multi_gpu training

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=rnspace_byt5
-#SBATCH --gres=gpu:a6000:1
+#SBATCH --gres=gpu:8
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=15G
+#SBATCH --mem-per-cpu=20G
 #SBATCH --mail-type=begin
 #SBATCH --mail-type=end
 #SBATCH --mail-user=rn3004@princeton.edu
 
-python /train.py
+srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 0 &
+srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 1 &
+srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 2 &
+srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 3 &
+srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 4 &
+srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 5 &
+srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 6 &
+srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 7 &
 
-cat slurm-*.out | more
