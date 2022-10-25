2ae88e3b83a22dd748d3c2927e260ba32018a1ec
Andrews2017 <3071003670@qq.com>
2022-10-25 18:31:51 -0400

    added scripts for multi_gpu training

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=rnspace_byt5
 #SBATCH --gres=gpu:8
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=8
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=20G
 #SBATCH --mail-type=begin
@@ -17,4 +17,4 @@ srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 4 &
 srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 5 &
 srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 6 &
 srun -N 1 -n 1 --gres=gpu:1 --exclusive python train.py 7 &
-
+wait
