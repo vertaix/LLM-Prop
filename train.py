b7b07b08d798567722f0209abe6d2fcfa1700b51
Andrews2017 <3071003670@qq.com>
2022-10-28 23:16:56 -0400

    updated

--- train.py
+++ train.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
     mat_descr_dir = f"data/property/{property_name}/mat_ids_description"
 
     n_classes = 1
-    batch_size = 8
+    batch_size = 2
     max_length = 1024
     
     train_data, valid_data, test_data = train_valid_test_split(
